853cdbd0d9188bf5e854e4386f2f6d2ed24c5fc5
pbnkp <341019+pbnkp@users.noreply.github.com>
2026-07-21 05:49:39 -0700

    smash 5.3: bump version + sha256 + test assertion (lockstep with pbnkp/smash main)

--- Formula/smash.rb
+++ Formula/smash.rb
@@ -2,8 +2,8 @@ class Smash < Formula
   desc "Unified encode/decode payload manager with AI semantic compression"
   homepage "https://github.com/pbnkp/smash"
   url "https://raw.githubusercontent.com/pbnkp/smash/main/smash"
-  sha256 "19ceaf85ccbf4cec5af8beb41e983a2bb2047dad000277bbf0079e9a7d66fc51"
-  version "5.0"
+  sha256 "5d190e60f02d6afffeb6638596eebf383d66d33924c33ffa54c103b9af2ead95"
+  version "5.3"
   license "MIT"
 
   depends_on "xz"
@@ -13,6 +13,6 @@ class Smash < Formula
   end
 
   test do
-    assert_match "smash v5.0", shell_output("#{bin}/smash --help 2>&1", 2)
+    assert_match "smash v5.3", shell_output("#{bin}/smash --help 2>&1", 2)
   end
 end
